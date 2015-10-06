a6e1b55de6b9bc44a20e68de7a867e9a0ef14a16
Davis Marques <dmarques@unimelb.edu.au>
2015-10-06 13:01:48 +1100

    Fix release task to ensure that the distributable is up to date.

--- conf/grunt/grunt-release.js
+++ conf/grunt/grunt-release.js
@@ -3,7 +3,7 @@
 module.exports = function (grunt) {
   grunt.registerTask('release', 'Create and tag a release',
     function () {
-      grunt.task.run(['checkbranch:master','bump']);
+      grunt.task.run(['checkbranch:master','compile','bump']);
     }
   );
 };
