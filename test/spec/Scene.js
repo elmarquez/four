a369ec2b8a18694131c05e43dd89b23d49b747b1
Davis Marques <dmarques@unimelb.edu.au>
2015-11-02 23:17:24 +1100

    Decompose controllers into simpler zoom, pan, etc. controls WIP

---
+++ test/spec/Scene.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Scene.
+ */
+describe('Scene', function () {
+
+    var scene;
+
+    beforeEach(function (done) {
+        scene = new FOUR.Scene();
+        done();
+    });
+
+});
