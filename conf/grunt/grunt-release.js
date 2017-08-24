820dc2d669d10d1fe125b4581ed5db9e4249b551
Davis Marques <dmarques@unimelb.edu.au>
2017-08-24 11:21:57 +1000

    Add version increment option to release task.

--- conf/grunt/grunt-release.js
+++ conf/grunt/grunt-release.js
@@ -1,9 +1,9 @@
-'use strict';
-
 module.exports = function (grunt) {
-    grunt.registerTask('release', 'Create and tag a release',
-        function () {
-            grunt.task.run(['checkbranch:master', 'compile', 'bump']);
-        }
-    );
+  'use strict';
+  grunt.registerTask('release', 'Create and tag a release',
+    function (increment) {
+      var bump = 'bump:' + (increment || 'patch');
+      grunt.task.run(['checkbranch:master', 'compile', bump]);
+    }
+  );
 };
